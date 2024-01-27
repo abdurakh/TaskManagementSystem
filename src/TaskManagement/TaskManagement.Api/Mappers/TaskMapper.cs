07f87a88a2825f516298005ff6ae3f5a097791d0
Azizbek Abdurahmonov <azizbek.abdura52@gmail.com>
2024-01-27 05:32:55 -0800

    task model mapper and Dto model added

---
+++ src/TaskManagement/TaskManagement.Api/Mappers/TaskMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TaskManagement.Api.Models.Dtos;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Api.Mappers;
+
+public class TaskMapper : Profile
+{
+    public TaskMapper()
+    {
+        CreateMap<TaskModel, TaskModelDto>().ReverseMap();
+    }
+}
---
+++ src/TaskManagement/TaskManagement.Api/Models/Dtos/TaskModelDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TaskManagement.Domain.Common.Enums;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Api.Models.Dtos;
+
+public class TaskModelDto
+{
+    public string Title { get; set; } = default!;
+
+    public string? Description { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public PriorityLevel Priority { get; set; }
+}
